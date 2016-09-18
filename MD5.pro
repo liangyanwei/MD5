bd31370b8f1e418b9c1d8c045ef950e17f8f6682
Liangyanwei <liangyanwei611@gmail.com>
2016-09-18 16:13:37 +0800

    First version
    - Edit ignore file, add Qt file filter

---
+++ MD5.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-09-13T12:05:26
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = MD5
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿#include <QByteArray>
+#include <QFile>
+#include <QCryptographicHash>
+#include <QDebug>
+
+int main(int argc, char **argv)
+{
+  if(argc == 2)
+  {
+    QString filePath = argv[1];
+    QFile theFile(filePath);
+
+    theFile.open(QIODevice::ReadOnly);
+    QByteArray ba = QCryptographicHash::hash(theFile.readAll(),QCryptographicHash::Md5);
+    theFile.close();
+
+    qDebug()<< "\n" << QString(ba.toHex().constData()).toUpper();
+  }
+
+  return 0;
+}
